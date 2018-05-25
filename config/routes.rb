db312751a9698667e1fc9685f7df78a16fa3d25b
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-24 21:32:27 -0500

    updated controller for current user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   get "/signup" => "users#new"
   post "/users" => "users#create"
+
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
   
   namespace :client do
     get '/products' => 'products#index'
