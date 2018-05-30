349a8170fd314c9708c3b36e70fa888723df2835
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-29 19:02:18 -0500

    added show, create, and new actions for orders on frontend

--- app/controllers/client/orders_controller.rb
+++ app/controllers/client/orders_controller.rb
@@ -1,26 +1,26 @@
 class Client::OrdersController < ApplicationController
-  def show
-    order_id = params[:id]
-    respose = Unirest.get("http://localhost:3000/api/orders/#{order_id}")
-    @order = respose.body
-    render 'show.html.erb'
-  end
-
+  
   def new
     render 'new.html.erb'
   end
 
   def create
-    order_params = {
+    client_params = {
                     product_id: params[:product_id],
                     quantity: params[:quantity]
                     } 
     response = Unirest.post(
                             "http://localhost:3000/api/orders/",
-                            parameters: product_params
+                            parameters: client_params
                             )
-    product = response.body
-    flash[:success] = "Successfully Created Product"
-    redirect_to "/client/products/#{ product["id"] }"
+    @order = response.body
+    flash[:success] = "Successfully Created Order"
+    redirect_to "/client/orders/#{@order['id']}"
+  end
+
+  def show
+    respose = Unirest.get("http://localhost:3000/api/orders/#{params[:id]}")
+    @order = respose.body
+    render 'show.html.erb'
   end
 end
